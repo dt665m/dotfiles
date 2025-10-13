d10eb73390e2527f2d0d2063cadff5d22bcf4ce3
dt665m <denistsai@aetheras.io>
2025-10-14 01:15:01 +0800

    fix: remove lazyvim bufferline catppuccin hack

--- lazyvim/lua/plugins/overrides.lua
+++ lazyvim/lua/plugins/overrides.lua
@@ -13,17 +13,6 @@ return {
     },
   },
 
-  -- #HACK for https://github.com/LazyVim/LazyVim/pull/6354
-  {
-    "akinsho/bufferline.nvim",
-    init = function()
-      local bufline = require("catppuccin.groups.integrations.bufferline")
-      function bufline.get()
-        return bufline.get_theme()
-      end
-    end,
-  },
-
   {
     "mrcjkb/rustaceanvim",
     opts = function(_, opts)
